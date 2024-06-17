de23009bf27ce502f48c88cab5b0d1bf3b9d34dd
Riocodex <onwukachibike@gmail.com>
2024-06-17 10:45:31 +0200

    handled viewing thhe books from api

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -8,8 +8,8 @@ const BackButton = ({destination = '/'}) => {
         <Link
             to={destination}
             className='bg-sky-800 text-white px-4 py-1 rounded-lg w-fit'
-            
         >
+            <BsArrowLeft className='text-2xl'/>
         </Link>
     </div>
   )
