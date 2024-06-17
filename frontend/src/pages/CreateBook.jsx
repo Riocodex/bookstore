3a535041fc9e99cdcede9d7c4274bf0d47f8c1c0
Riocodex <onwukachibike@gmail.com>
2024-06-17 10:45:51 +0200

    handled viewing a single book from api

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -54,7 +54,7 @@ const CreateBook = () => {
           className='border-2 border-gray-500 px-4 py-2 w-full'
           />
         </div>
-        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}></button>
+        <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}></buttn>
       </div>
     </div>
   )
